fead9b08a90463eb401015333f8180f539540d59
sangrammarge <sangrammarge@gmail.com>
2026-08-29 21:18:15 +0530

    Add files via upload

---
+++ MARGE9.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+   // This is a single-line comment...
+
+   /* 
+    This is a multi-line comment.
+    It can be span across multiple lines.
+    */
+
+    return 0;
+}	
