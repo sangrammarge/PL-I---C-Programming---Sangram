12183537af49d8789936beba343ad49072da92c6
sangrammarge <sangrammarge@gmail.com>
2026-08-29 20:56:30 +0530

    Add files via upload

---
+++ MARGE2.C
@@ -0,0 +1,23 @@
+# include<stdio.h>
+int main()
+{
+  int rollnum;
+  float per;
+  char grade;
+
+  printf("\n Enter Student Roll Number:");
+  scanf("%d",&rollnum);
+
+  printf("\n Enter Student Percentage:");
+  scanf("%f",&per);
+
+  printf("\n Enter Student Grade:");
+  scanf(" %c",&grade);
+
+  printf("\n ----Student Information----\n");
+  printf("\n Roll Number is: %d", rollnum);
+  printf("\n Percentage is : %f", per);
+  printf("\n Grade         : %c", grade);
+
+  return 0;
+}
