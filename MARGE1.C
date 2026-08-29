f77f91cf2098905205fb8150a544e94862e41771
sangrammarge <sangrammarge@gmail.com>
2026-08-29 16:11:57 +0530

    Add files via upload

---
+++ MARGE1.C
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+ int main()
+{
+  int rollnum;
+  float per;
+  char grade;
+
+  rollnum = 101;
+  per = 89.75;
+  grade = 'A';
+
+  printf("----Student Information----\n");
+  printf("\n Roll Number:%d", rollnum);
+  printf("\n Percentage:%f", per);
+  printf("\n Grade:%c", grade):
+
+return 0;
