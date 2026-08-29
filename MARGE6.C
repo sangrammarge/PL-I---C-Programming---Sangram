c3b1372984ce09aa3ea9f6585553234d928ce55d
sangrammarge <sangrammarge@gmail.com>
2026-08-29 21:16:18 +0530

    Add files via upload

---
+++ MARGE6.C
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() 
+{
+  int num1, num2, num3, total;
+  float average;
+
+  printf("Enter First Number:");
+  scanf("%d",& num1);
+  printf("Enter Second Number:");
+  scanf("%d",& num2);
+  printf("Enter Third Number:");
+  scanf("%d",& num3);  
+
+  total = (num1+num2+num3);
+  average = total/3;
+  printf( "\n Average = %f",average);
+
+      
+       return 0;
+}
