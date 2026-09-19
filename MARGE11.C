a38945be04c0468624ee78611e67d1080d31720d
sangrammarge <sangrammarge@gmail.com>
2026-09-19 18:50:00 +0530

    Add files via upload

---
+++ MARGE11.C
@@ -0,0 +1,24 @@
+//Program(11):write a program to check whether a give number is even or odd.
+
+#include <stdio.h>
+int main()
+
+{
+int num;
+printf("Enter your number: ");
+scanf("%d", &num);
+
+if (num % 2 == 0)
+
+{
+  printf("The number is Even.");
+}
+
+else
+{
+
+  printf("The number is Odd.");
+}
+return 0;
+
+}
