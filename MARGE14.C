7c56952b5f3a7d40160a87b5c72733357677a77c
sangrammarge <sangrammarge@gmail.com>
2026-09-20 16:25:45 +0530

    Minor fix in documentation section

--- MARGE14.C
+++ MARGE14.C
@@ -1,5 +1,5 @@
 /*Program(14):write a program to generate the multiple table of the any number using while loop, do while loop, and for loop.
-solution(1):using while loop/*
+solution(1):using while loop*/
 #include<stdio.h>
 int main()
 {
